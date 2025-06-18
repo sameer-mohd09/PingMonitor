f569e8ac25010fa9102f6884e8f35c19389936b5
Sameer Mohammed <personalsameer07@gmail.com>
2025-06-18 20:46:09 +0530

    Refactor KV storage initialization and improve error handling in getAllSettings function

--- kv/kv.ts
+++ kv/kv.ts
@@ -1,19 +1,20 @@
+import { assert } from "https://deno.land/std@0.203.0/assert/assert.ts";
 import { SiteSetting } from "../types.ts";
-// import { assert } from "https://deno.land/std@0.203.0/assert/mod.ts";
 
 
-const kv = await Deno.openKv();
+const kv = await Deno.openKv(".data/kv");
+// const kv = await Deno.openKv();
 
 export async function saveSetting(key: string, value: SiteSetting) {
   await kv.set(["settings", key], value);
 }
 
-// export async function getSetting(key: string): Promise<SiteSetting> {
-//   const result = await kv.get<SiteSetting>(["settings", key]);
-//   assert(result.value, `Setting not found for key: ${key}`);
-//   return result.value;
+export async function getSetting(key: string): Promise<SiteSetting> {
+  const result = await kv.get<SiteSetting>(["settings", key]);
+  assert(result.value, `Setting not found for key: ${key}`);
+  return result.value;
 
-// }
+}
 
 export async function getAllSettings(): Promise<SiteSetting[]>{
    const settings=[];
@@ -22,7 +23,8 @@ export async function getAllSettings(): Promise<SiteSetting[]>{
   for await (const iter of kv.list<SiteSetting>({prefix:["settings"]})){
     settings.push(iter.value );
    }
-  }catch(err){
+  }
+  catch(err){
     if (err instanceof Error) {
       console.error("Failed to fetch all settings from KV:", err.message);
     } else {
@@ -31,3 +33,8 @@ export async function getAllSettings(): Promise<SiteSetting[]>{
    }
   return settings;
 } 
+
+
+export async function deleteSettings(key:string){
+  await kv.delete(["settings",key]);
+}
